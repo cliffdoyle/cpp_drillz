194cbe7a9ab9109a25011d16d9bb631120c0fd1e
cliffdoyle <oyoocliff471@gmail.com>
2026-07-03 12:29:40 +0300

    Add main function to test the naive class implementation

--- computations/exercises/enums_drill.cpp
+++ computations/exercises/enums_drill.cpp
@@ -1,54 +1,63 @@
-//Classes
-# include <string>
-# include <iostream>
-# include <vector>
-# include <limits>
-# include <algorithm>
+// Classes
+#include <string>
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
 
-class Name_pairs {
+class Name_pairs
+{
 public:
-	void read_names(std::vector<std::string>&);
-	void read_ages(std::vector<std::string>&);
-	void print(std::vector<std::string>&,std::vector<double>&);
-	void sort(std::vector<std::string>&);
+	void read_names(std::vector<std::string> &);
+	void read_ages(std::vector<std::string> &);
+	void print(std::vector<std::string> &);
+	void sort(std::vector<std::string> &);
+
 private:
 	std::vector<std::string> name;
 	std::vector<double> age;
-
 };
 
-void Name_pairs::read_names(std::vector<std::string>& n)
+void Name_pairs::read_names(std::vector<std::string> &n)
 {
 	for (std::string s : n)
 	{
 		std::cout << "names in the name_vectors: " << s << "\n";
 	}
-
 }
 
-void Name_pairs::read_ages(std::vector<std::string>& n)
-{	
+void Name_pairs::read_ages(std::vector<std::string> &n)
+{
 	int ag{};
-	for (int i = 0;i < n.size();i++)
+	for (int i = 0; i < n.size(); i++)
 	{
-		std::cout << "Name: " << n[0] << "Kindly put your age: " << "\n";
+		std::cout << "Name: " << n[i] << "Kindly put your age: " << "\n";
 		std::cin >> ag;
 		Name_pairs::age.push_back(ag);
-
 	}
-
 }
 
-void Name_pairs::sort(std::vector<std::string>& v)
+void Name_pairs::sort(std::vector<std::string> &v)
 {
-	std::sort(v.begin(),v.end());
-
+	std::sort(v.begin(), v.end());
 }
 
-void Name_pairs::print(std::vector<std::string>& n,std::vector<double>& a)
+void Name_pairs::print(std::vector<std::string> &n)
 {
 	// std::numeric_limits(n);
+	for (int i = 0; i < n.size(); i++)
+	{
+		// for (int j = 0; j < age.size(); j++)
+		// {
+			std::cout << n[i] << age[i] << "\n";
+		// }
+	}
+}
 
-
-
+int main()
+{
+	std::vector<std::string> names{"caro", "jayne", "milka", "roy"};
+	Name_pairs namey;
+	namey.read_ages(names);
+	namey.print(names);
 }
