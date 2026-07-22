64c44d8415970ed7fe473500bf2ec00c0fb11b9a
cliffdoyle <oyoocliff471@gmail.com>
2026-07-22 23:39:48 +0300

    Add linkedlist implementation

---
+++ memory/mem.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// void sizes(char ch, int i, double *p)
+// {
+//     cout << "the size of char is " << sizeof(char) << ' ' << sizeof(ch) << '\n';
+//     cout << "the size of int is " << sizeof(int) << ' ' << sizeof(i) << '\n';
+//     cout << "the size of int* is " << sizeof(int *) << ' ' << sizeof(p) << '\n';
+// }
+
+// class Vector
+// {
+// public:
+//     Vector(int s);
+//     ~Vector();
+
+// private:
+//     int sz;
+//     double *elem;
+// };
+
+// Vector::Vector(int s)
+//     : sz{s}, elem{new double[s]}
+// {
+//     for (int i = 0; i < s; ++i)
+//     {
+//         elem[i] = 3;
+//     }
+//     for (int a; a < sz; ++a)
+//     {
+
+//         cout << elem[a] << "\n";
+//     }
+// }
+
+// Vector::~Vector()
+// {
+//     delete[] elem;
+// }
+
+// // ostream &operator<<(ostream os, Vector p)
+// // {
+// //     for (int a : p)
+// //     {
+
+// //         os << a << "\n";
+// //     }
+// // }
+// class Animal
+// {
+// public:
+//     void eat()
+//     {
+//         cout << "Eating\n";
+//     }
+// };
+// class Dog : public Animal
+// {
+// };
+
+// // Linked lists
+// struct Link
+// {
+//     Link(const string &v, Link *p = nullptr, Link *s = nullptr)
+//         : value{v}, prev{p}, succ{s} {}
+
+//     string value;
+//     Link *prev;
+//     Link *succ;
+// };
+
+// ostream &operator<<(ostream &os, Link *l)
+// {
+//     os << "name of node: " << l->value << '\n';
+//     os << "name of the successor node: " << l->succ->value << '\n';
+//     os << "name of the previous node: " << l->prev->value << '\n';
+//     return os;
+// }
+
+// int main()
+// {
+//     Link *norse = new Link{"Kratos", nullptr, nullptr};
+//     Link *norse2 = new Link{"Thor", nullptr, norse};
+//     Link *norse3 = new Link{"heyoo", nullptr, norse2};
+//     norse2->prev = norse3;
+//     norse->succ = norse2;
+//     norse->prev = norse3;
+//     norse3->prev = norse;
+
+//     cout << "linkedlist stuff: " << norse2 << "\n";
+//     cout << "linkedlist stuff: " << norse << "\n";
+//     cout << "linkedlist stuff: " << norse3 << "\n";
+
+//     // double *d = nullptr;
+//     // sizes('a', 34, d);
+
+//     // vector<int> *p = new vector<int>;
+//     // cout << "address of p" << p << '\n';
+//     // *p = {1, 2, 3, 3, 4, 5};
+//     // // cout << "contents of p" << (*p) << '\n';
+//     // (*p)[2] = 39;
+
+//     // for (int p : *p)
+//     // {
+//     //     cout << "contents of p" << p << '\n';
+//     // }
+
+//     // double *i = new double[4];
+//     // *i = {1};
+//     // cout << "contents of double:" << *i << "\n";
+
+//     // delete[] p;
+//     Vector v(5);
+//     Dog *d;
+//     d->eat();
+// }
+
+struct Link
+{
+    Link(const string &v, Link *p = nullptr, Link *s = nullptr)
+        : value{v}, prev{p}, succ{s} {}
+
+    string value;
+    Link *prev;
+    Link *succ;
+};
+
+Link *insert(Link *p, Link *n)
+{
+    if (n == nullptr)
+    {
+        return p;
+    }
+
+    if (p == nullptr)
+    {
+        return n;
+    }
+
+    n->succ = p; // p comes after n
+    if (p->prev)
+    {
+        p->prev->succ = n;
+    }
+    n->prev = p->prev;
+    p->prev = n;
+    return n;
+}
